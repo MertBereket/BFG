df6e6ec3701e553fbe559aadecc446cafa9b1c0d
aktan2778 <66529142+aktan2778@users.noreply.github.com>
2021-01-16 15:36:13 +0300

    feat : GUI Elements for adding new film,series,games added

--- Module/src/sample/Main.java
+++ Module/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("BFGReview");
         primaryStage.setScene(new Scene(root, 707, 525));
         primaryStage.show();
     }
